fddee42dfe45218174cc14595a95baa90c93cdb2
domb <woon4910@gmail.com>
2022-06-06 20:33:44 +0900

    feat: MVC, grouping

---
+++ RPSGame/RPSGame/Extension/Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Extension.swift
+//  RPSGame
+//
+//  Created by 서동운 on 2022/06/05.
+//
+
+import UIKit
+// MARK: - UIViewController
+
+extension UIViewController {
+    
+    func showMessage(title: String, message: String?, firstAction: String?, completion: ((UIAlertAction) -> Void)?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let firstAction = UIAlertAction(title: firstAction, style: .default, handler: completion)
+        alert.addAction(firstAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+// MARK: - UIView
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, paddingTop: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+       
+    }
+}
---
+++ RPSGame/RPSGame/Model/GameData.swift
@@ -0,0 +1,12 @@
+//
+//  GameData.swift
+//  RPSGame
+//
+//  Created by 서동운 on 2022/06/05.
+//
+
+import Foundation
+
+struct GameData {
+    
+}
