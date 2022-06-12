c08c35cdc3bbac9881866351ffb0c5322bbaa13c
Brain2931 <sem789456@gmail.com>
2022-06-12 23:00:48 +0900

    feat: add stack view for login buttons

--- RPSGame/RPSGame/Controller/LogInViewController.swift
+++ RPSGame/RPSGame/Controller/LogInViewController.swift
@@ -8,17 +8,24 @@
 import UIKit
 import Firebase
 
-final class LogInViewController: UIViewController {
+
+final class LoginViewController: UIViewController {
 
     private let backgroundView = UIImageView()
     private let mainLabel = UILabel()
+    private let kakaoLoginButton = UIButton()
+    private let loginButtonStack = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupBackgroundView()
         setupMainLabel()
+        setupStackView()
+        setupLoginButtons()
+        
         appear()
+        
     }
     
     private func setupBackgroundView() {
@@ -37,14 +44,14 @@ final class LogInViewController: UIViewController {
         mainLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(mainLabel)
         
-        let mainLabelAutoLayout = [
+        let constraints = [
         mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
         mainLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 150),
         mainLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
         mainLabel.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4),
         ]
 
-        NSLayoutConstraint.activate(mainLabelAutoLayout)
+        NSLayoutConstraint.activate(constraints)
         
         mainLabel.backgroundColor = .systemGray
         mainLabel.layer.masksToBounds = true
@@ -56,17 +63,44 @@ final class LogInViewController: UIViewController {
         mainLabel.font = UIFont.boldSystemFont(ofSize: 50)
     }
     
-//    private func setupLogInButton() {
-//        let stackView = UIStackView()
-//
-//    }
+    private func setupLoginButtons() {
+        loginButtonStack.addArrangedSubview(kakaoLoginButton)
+
+        kakaoLoginButton.setImage(UIImage(named: "kakaoLogIn"), for: .normal)
+        kakaoLoginButton.addTarget(self, action: #selector(kakaoLogin), for: .touchUpInside)
+        kakaoLoginButton.alpha = 0
+
+        kakaoLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        kakaoLoginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        kakaoLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func setupStackView() {
+        view.addSubview(loginButtonStack)
+        
+        loginButtonStack.axis = .vertical
+        loginButtonStack.spacing = 20.0
+        loginButtonStack.alignment = .center
+        loginButtonStack.distribution = .fillEqually
+        
+        loginButtonStack.translatesAutoresizingMaskIntoConstraints = false
+        loginButtonStack.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        loginButtonStack.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -150).isActive = true
+        
+    }
+    
     
     private func appear() {
         UIView.animate(withDuration: 3) {
             self.backgroundView.alpha = 1
             self.mainLabel.alpha = 1
+            self.kakaoLoginButton.alpha = 1
             self.mainLabel.backgroundColor = UIColor.systemGray.withAlphaComponent(0.4)
         }
+    }
+    
+    @objc func kakaoLogin() {
+        print("dhdld")
     }
 }
 
