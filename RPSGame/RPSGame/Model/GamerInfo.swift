50e7e5b4b0693263209a9eb79a84d010dfa754a4
Brain2931 <sem789456@gmail.com>
2022-06-14 11:37:30 +0900

    test: test

--- RPSGame/RPSGame/Model/GamerInfo.swift
+++ RPSGame/RPSGame/Model/GamerInfo.swift
@@ -9,8 +9,9 @@ import Foundation
 
 
 struct GamerInfo {
-    var name: Int
+    var name: String
     var choice: RPS?    // lazy는 optional일 때 못씀!
     var wantsGameStart: Bool
     let gitTest: String
+    let gitTest2: String
 }
